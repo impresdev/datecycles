433762a68c91aa2ad3a7e4b347d7869a441402a8
impresdev <dev@impresario.net.au>
2023-11-25 06:37:40 +0000

    Add LICENSE

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='datecycles',
     version='0.1.0',
+    description='A Python package designed for generating a series of dates based on specific cycle periods.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/impresdev/datecycles',
+    author='impresdev',
+    author_email='dev@impresario.net.au',
+    license='MIT',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -10,7 +17,6 @@ setup(
         ],
     },
     install_requires=[
-        # Add your dependencies here, for example:
         'python-dateutil',
     ],
 )
