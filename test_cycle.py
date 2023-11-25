9d2fe45929a7fa4cbb65c9cef9109539bead362b
Joel <Dev>
2023-11-25 02:35:35 +0000

    Added more tests

--- test_cycle.py
+++ test_cycle.py
@@ -2,36 +2,57 @@ import unittest
 from cycle import get_last_day_of_month, add_months, generate_cycle_dates
 from datetime import datetime
 
-class DateCycleTest(unittest.TestCase):
+class TestCycleModule(unittest.TestCase):
 
-    def test_get_last_day_of_month(self):
+    # Tests for get_last_day_of_month
+    def test_get_last_day_regular_month(self):
         self.assertEqual(get_last_day_of_month(2023, 1), datetime(2023, 1, 31))
+
+    def test_get_last_day_february_non_leap_year(self):
         self.assertEqual(get_last_day_of_month(2023, 2), datetime(2023, 2, 28))
-        self.assertEqual(get_last_day_of_month(2024, 2), datetime(2024, 2, 29))  # Leap year
 
-    def test_add_months(self):
-        self.assertEqual(add_months(datetime(2023, 1, 30), 1), datetime(2023, 2, 28))
-        self.assertEqual(add_months(datetime(2023, 11, 30), 2), datetime(2024, 1, 30))
+    def test_get_last_day_february_leap_year(self):
+        self.assertEqual(get_last_day_of_month(2024, 2), datetime(2024, 2, 29))
 
-    def test_monthly_cycle(self):
-        self.assertEqual(generate_cycle_dates("2023-10-01", 12, 3), ['2023-10-01', '2023-11-01', '2023-12-01'])
+    # Tests for add_months
+    def test_add_months_zero_month(self):
+        self.assertEqual(add_months(datetime(2023, 1, 15), 0), datetime(2023, 1, 15))
 
-    def test_four_weekly_cycle(self):
-        self.assertEqual(generate_cycle_dates("2023-10-01", 13, 3), ['2023-10-01', '2023-10-29', '2023-11-26'])
+    def test_add_months_normal(self):
+        self.assertEqual(add_months(datetime(2023, 1, 15), 1), datetime(2023, 2, 15))
 
-    def test_fortnightly_cycle(self):
-        self.assertEqual(generate_cycle_dates("2023-10-01", 26, 6), ['2023-10-01', '2023-10-15', '2023-10-29', '2023-11-12', '2023-11-26', '2023-12-10'])
+    def test_add_months_year_rollover(self):
+        self.assertEqual(add_months(datetime(2023, 12, 15), 1), datetime(2024, 1, 15))
 
-    def test_weekly_cycle(self):
-        self.assertEqual(generate_cycle_dates("2023-10-01", 52, 6), ['2023-10-01', '2023-10-08', '2023-10-15', '2023-10-22', '2023-10-29', '2023-11-05'])
+    # Tests for generate_cycle_dates
+    def test_generate_cycle_monthly(self):
+        expected_dates = ['2023-10-01', '2023-11-01', '2023-12-01']
+        self.assertEqual(generate_cycle_dates("2023-10-01", 12, 3), expected_dates)
 
-    def test_adjust_date_for_invalid_day(self):
-        self.assertEqual(generate_cycle_dates("2023-02-29", 12, 4), ['2023-02-28', '2023-03-28', '2023-04-28', '2023-05-28'])
+    def test_generate_cycle_four_weekly(self):
+        expected_dates = ['2023-10-01', '2023-10-29', '2023-11-26']
+        self.assertEqual(generate_cycle_dates("2023-10-01", 13, 3), expected_dates)
 
-    def test_adjust_date_for_invalid_month_day(self):
-        self.assertEqual(generate_cycle_dates("2023-11-32", 12, 4), ['2023-11-30', '2023-12-30', '2024-01-30', '2024-02-29'])
+    def test_generate_cycle_fortnightly(self):
+        expected_dates = ['2023-10-01', '2023-10-15', '2023-10-29', '2023-11-12', '2023-11-26', '2023-12-10']
+        self.assertEqual(generate_cycle_dates("2023-10-01", 26, 6), expected_dates)
 
-    # Add any other tests for generate_cycle_dates function or other functions as needed
+    def test_generate_cycle_weekly(self):
+        expected_dates = ['2023-10-01', '2023-10-08', '2023-10-15', '2023-10-22', '2023-10-29', '2023-11-05']
+        self.assertEqual(generate_cycle_dates("2023-10-01", 52, 6), expected_dates)
+
+    def test_generate_cycle_last_day_of_february(self):
+        expected_dates = ['2023-02-28', '2023-03-28', '2023-04-28', '2023-05-28']
+        self.assertEqual(generate_cycle_dates("2023-02-29", 12, 4), expected_dates)
+
+    def test_generate_cycle_adjust_last_day_of_month(self):
+        expected_dates = ['2023-11-30', '2023-12-30', '2024-01-30', '2024-02-29', '2024-03-30']
+        self.assertEqual(generate_cycle_dates("2023-11-32", 12, 5), expected_dates)
+
+    def test_generate_cycle_invalid_cycle_period(self):
+        with self.assertRaises(ValueError):
+            generate_cycle_dates("2023-01-01", 5, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
