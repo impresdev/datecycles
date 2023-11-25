01e0c76cc02693d1735138d60a875af76b620e32
impresdev <dev@impresario.net.au>
2023-11-25 07:43:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datecycles',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python package designed for generating a series of dates based on specific cycle periods.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
