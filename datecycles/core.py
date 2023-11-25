e96918209faec152c0e87a69c6fb93051383c2f6
Joel <Dev>
2023-11-25 05:10:42 +0000

    Add cli entry, comments and remove prints

--- datecycles/core.py
+++ datecycles/core.py
@@ -4,6 +4,20 @@ from dateutil.relativedelta import relativedelta
 
 
 def get_last_day_of_month(year, month):
+    """
+    Calculate the last day of a specified month and year.
+
+    Args:
+        year (int): The year in the Gregorian calendar.
+        month (int): The month as an integer, where January is 1 and December is 12.
+
+    Returns:
+        datetime: A datetime object representing the last day of the specified month and year.
+
+    Raises:
+        TypeError: If the inputs are not integers.
+        ValueError: If the month is not in the range 1-12.
+    """
     if not isinstance(year, int) or not isinstance(month, int):
         raise TypeError("Year and month must be integers")
 
@@ -14,6 +28,20 @@ def get_last_day_of_month(year, month):
     return datetime(year, month, last_day)
 
 def add_months(start_date, months):
+    """
+    Add a specific number of months to a given date.
+
+    Args:
+        start_date (datetime): The starting date to which months will be added.
+        months (int): The number of months to add to the start_date.
+
+    Returns:
+        datetime: A datetime object representing the date after adding the specified number of months.
+
+    Raises:
+        TypeError: If start_date is not a datetime object or if months is not an integer.
+        ValueError: If the number of months to add is negative.
+    """
     if not isinstance(start_date, datetime):
         raise TypeError("start_date must be a datetime object")
 
@@ -22,7 +50,16 @@ def add_months(start_date, months):
     return start_date + relativedelta(months=+months)
 
 def parse_start_date(start_date):
-    """Parse and validate the start date."""
+    """
+    Parse a string representing a date and return a datetime object. If the day of the month
+    is invalid, adjust it to the last valid day of the month.
+
+    Args:
+        start_date (str): A string representing a date in 'YYYY-MM-DD' format.
+
+    Returns:
+        datetime: A datetime object representing the parsed date.
+    """
     try:
         return datetime.strptime(start_date, "%Y-%m-%d")
     except ValueError:
@@ -30,6 +67,23 @@ def parse_start_date(start_date):
         return get_last_day_of_month(year, month)
 
 def generate_cycle_dates(start_date, cycle_period, cycle_count):
+    """
+    Generate a list of dates in a cycle starting from a given date. The function supports various
+    cycle periods such as weekly, fortnightly, 4-weekly, monthly, and quarterly.
+
+    Args:
+        start_date (str): The starting date of the cycle in 'YYYY-MM-DD' format.
+        cycle_period (int): The period of the cycle. Acceptable values are 52 (weekly),
+                            26 (fortnightly), 13 (4-weekly), 12 (monthly), and 4 (quarterly).
+        cycle_count (int): The number of cycles to generate.
+
+    Returns:
+        list: A list of strings where each string is a date in 'YYYY-MM-DD' format, representing the cycle dates.
+
+    Raises:
+        TypeError: If the start_date is not a string, or if cycle_period and cycle_count are not integers.
+        ValueError: If the cycle count is negative or if the cycle period is not one of the accepted values.
+    """
     if not isinstance(start_date, str):
         raise TypeError("start_date must be a string")
 
@@ -55,18 +109,3 @@ def generate_cycle_dates(start_date, cycle_period, cycle_count):
     cycle_dates = [cycle_strategies[cycle_period](start_date_obj, i).strftime("%Y-%m-%d") for i in range(cycle_count)]
 
     return cycle_dates
-
-# Example usage
-print(generate_cycle_dates("2023-10-01", 12, 3))  # monthly for 3 cycles
-print(generate_cycle_dates("2023-10-01", 13, 3))  # 4-weekly for 3 cycles
-print(generate_cycle_dates("2023-10-01", 26, 6))  # fortnightly for 6 cycles
-print(generate_cycle_dates("2023-10-01", 52, 6))  # weekly for 6 cycles
-print(generate_cycle_dates("2023-02-29", 12, 4))  # Adjusts to correct last day of Feb
-print(generate_cycle_dates("2023-11-32", 12, 5))  # Adjusts to last day of month
-
-# ['2023-10-01', '2023-11-01', '2023-12-01']
-# ['2023-10-01', '2023-10-29', '2023-11-26']
-# ['2023-10-01', '2023-10-15', '2023-10-29', '2023-11-12', '2023-11-26', '2023-12-10']
-# ['2023-10-01', '2023-10-08', '2023-10-15', '2023-10-22', '2023-10-29', '2023-11-05']
-# ['2023-02-28', '2023-03-28', '2023-04-28', '2023-05-28']
-# ['2023-11-30', '2023-12-30', '2024-01-30', '2024-02-29', '2024-03-30']
--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name='datecycles',
     version='0.1.0',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'datecycles=datecycles.cli:main',
+        ],
+    },
     install_requires=[
         # Add your dependencies here, for example:
         'python-dateutil',
