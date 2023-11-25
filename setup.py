0f109ec96bea0abb2347f3372df926b2fbdf7e03
impresdev <dev@impresario.net.au>
2023-11-25 07:43:38 +0000

    Restructure project into a package format

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datecycles',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here, for example:
+        'python-dateutil',
+    ],
+)
